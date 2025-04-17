ec9c2facaa7b2ccf9c6dd79e889d84cd3c07336b
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-04-16 22:06:02 -0300

    feat: create theme switcher component (without logic)

---
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -0,0 +1,20 @@
+import "./ThemeSwitcher.css";
+
+// icons importation
+import { IoSunnyOutline } from "react-icons/io5";
+import { PiMoonLight } from "react-icons/pi";
+
+const ThemeSwitcher: React.FC = () => {
+  return (
+    <div className="theme-switcher">
+      <span>
+        <IoSunnyOutline className="theme-switcher__icon" />
+      </span>
+      <span>
+        <PiMoonLight className="theme-switcher__icon theme-switcher__icon--active" />
+      </span>
+    </div>
+  );
+};
+
+export default ThemeSwitcher;

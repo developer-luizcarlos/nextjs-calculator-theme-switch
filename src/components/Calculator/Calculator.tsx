d4b460dc6a7074e726f2ea95ef825657eba38b2e
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-04-16 21:08:15 -0300

    feat: implement Button component on Calculator component
    A set of Button's were implemented on the Calculator component
    and their initial/basic property values was also defined.

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,7 +1,7 @@
 import "./Calculator.css";
 
 // components importation
-// import Button from "../Button/Button";
+import Button from "../Button/Button";
 
 const Calculator: React.FC = () => {
   return (
@@ -11,7 +11,28 @@ const Calculator: React.FC = () => {
         <span className="calc__last-result">5.55</span>
         <input type="text" disabled value={"0.00"} className="calc__display" />
       </header>
-      <div className="calc__pad"></div>
+      <div className="calc__pad">
+        <Button label="AC" role="action" />
+        <Button label="&#8730;" role="action" />
+        <Button label="%" role="action" />
+        <Button label="+" role="operational" />
+        <Button label="7" role="normal" />
+        <Button label="8" role="normal" />
+        <Button label="9" role="normal" />
+        <Button label="X" role="operational" />
+        <Button label="4" role="normal" />
+        <Button label="5" role="normal" />
+        <Button label="6" role="normal" />
+        <Button label="-" role="operational" />
+        <Button label="1" role="normal" />
+        <Button label="2" role="normal" />
+        <Button label="3" role="normal" />
+        <Button label="+" role="operational" />
+        <Button label="&#10226;" role="normal" />
+        <Button label="0" role="normal" />
+        <Button label="." role="normal" />
+        <Button label="=" role="operational" />
+      </div>
     </article>
   );
 };

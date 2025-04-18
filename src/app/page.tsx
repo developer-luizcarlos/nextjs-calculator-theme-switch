3b931e1d04770f84615f315a436e1b7a2e5b3399
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-04-18 11:00:44 -0300

    feat: implement theme context to page.tsx to define which theme will be applied
    The context used to define the theme value between light or dark and the
    function to toggle it was created in a context file and, now, it was
    used in the page.tsx file to define which className, light-theme or dark-theme,
    it will be used.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
+"use client";
+// Stylesheet importation
 import "./page.css";
 
 // Components importation
 import Calculator from "@/components/Calculator/Calculator";
 
-const page: React.FC = () => {
+// Hooks importation
+import { useContext } from "react";
+
+// Theme context importation
+import { ThemeContextObject } from "@/contexts/ThemeContext";
+
+const Page: React.FC = () => {
+  const { theme } = useContext(ThemeContextObject)!;
+
   return (
-    <div className="page">
+    <div className={theme === "light" ? "page light-theme" : "page dark-theme"}>
       <Calculator />
     </div>
   );
 };
 
-export default page;
+export default Page;

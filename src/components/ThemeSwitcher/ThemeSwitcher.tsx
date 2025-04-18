f8f77a66826c81cb006d47ea5485313fee964b33
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-04-18 16:48:59 -0300

    fix: remove unnecessary structures and styles
    Unnecessary wrapper and hover transitions were removed
    from the component. A dynamic className based on the
    theme context value was implemented.

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,18 +1,37 @@
+"use client";
+
+// Stylesheet importation
 import "./ThemeSwitcher.css";
+
+// Hooks importation
+import { useContext } from "react";
+
+// Theme context importation
+import { ThemeContextObject } from "@/contexts/ThemeContext";
 
 // icons importation
 import { IoSunnyOutline } from "react-icons/io5";
 import { PiMoonLight } from "react-icons/pi";
 
 const ThemeSwitcher: React.FC = () => {
+  const { theme } = useContext(ThemeContextObject)!;
+
   return (
     <div className="theme-switcher">
-      <span className="theme-switcher__span">
-        <IoSunnyOutline className="theme-switcher__icon" />
-      </span>
-      <span className="theme-switcher__span">
-        <PiMoonLight className="theme-switcher__icon theme-switcher__icon--active" />
-      </span>
+      <IoSunnyOutline
+        className={
+          theme === "light"
+            ? "theme-switcher__icon theme-switcher__icon--active"
+            : "theme-switcher__icon"
+        }
+      />
+      <PiMoonLight
+        className={
+          theme === "dark"
+            ? "theme-switcher__icon theme-switcher__icon--active"
+            : "theme-switcher__icon"
+        }
+      />
     </div>
   );
 };
